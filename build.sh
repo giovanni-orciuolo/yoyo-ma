465bff66e81398d146bbb37c66696abbddc1b183
double v <DoubleHub@users.noreply.github.com>
2019-03-11 01:30:50 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-rm -rf YoYo-Ma
-git clone git@github.com:DoubleHub/YoYo-Ma.git
 docker build --build-arg token=YOUR_TOKEN_HERE -t yoyo-discord-image YoYo-M$docker run -d --name yoyo-discord-app yoyo-discord-image
 docker run -d --name yoyo-discord-app yoyo-discord-image
